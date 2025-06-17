49d397d1630e6d899f5b91a78326d199bdb9ebeb
kilan080 <kilanioluwapheranmie@gmail.com>
2025-06-17 17:51:07 +0100

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,155 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { SiPinetwork } from "react-icons/si";
 
-export default function Home() {
+
+  const inputCheckBoxStyle = {
+  border: "2px solid red",
+  outline: "2px solid black",
+  accentColor: "red",
+  outlineOffset: "2px",
+};
+
+const inputCheckBoxStyleErr = {
+  outline: "2px solid red",
+};
+
+  // return (
+
+  export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [hasNoSymbolErr, setHasNoSymbolErr] = useState(false);
+  const [greaterThanSixErr, setGreaterThanSixErr] = useState(false);
+  const [hasUpperCase, setHasUpperCase] = useState(false);
+  const [isLongEnough, setIsLongEnough] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+
+  const checkUsernameValidation = (name) => {
+    const hasNoSymbol = /^[a-zA-Z0-9]*$/.test(name);
+    const greaterThanSix = username.length > 6;
+    // Check if username is greater than 6
+    // check if username has no character
+    setGreaterThanSixErr(greaterThanSix);
+    setHasNoSymbolErr(hasNoSymbol);
+
+    // setIsValid(hasNoSymbol && greaterThanSix);
+    // if (hasNoSymbol) {
+    //   setHasNoSymbolErr(true);
+    //   return false;
+    // }
+    // if(greaterThanSix) {
+    //   setGreaterThanSixErr(true);
+    //   return false;
+    // }
+    // return true
+    return hasNoSymbol && greaterThanSix;
+  };
+
+  const checkPasswordValidation = (pwd) => {
+    const hasNum = /\d/.test(pwd);
+    const hasCapital = /[A-Z]/.test(pwd);
+    const isLong = pwd.length > 6;
+
+
+    setHasNumber(hasNum);
+    setHasUpperCase(hasCapital);
+    setIsLongEnough(isLong);
+
+
+    // check if password is greater 6
+    // check if password has atleast one character
+    // check if password has atleast one capital letter
+    // return true;
+    return hasNum && hasCapital && isLong;
+  };
+
+  const login = () => {
+    if (checkUsernameValidation(username) && checkPasswordValidation(password)) {
+      setIsLoggedIn(true);
+    } else {
+      alert("Check input and try again");
+    }
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+  const symbolErr = hasNoSymbolErr ? inputCheckBoxStyleErr : {};
+  console.log("hasNoSymbolErr", hasNoSymbolErr);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="App">
+      {isLoggedIn ? (
+        <div>
+           <div className="bg-gray-50 flex flex-col  justify-center items-center text-center">
+            <h1 className="text-3xl mt-5 pt-4">Profile Page</h1>
+            <img className="flex items-center object-cover justify-center object-center rounded-full h-60 w-60 mt-2" src='./profImage.webp' alt="" />
+            <h2 className="text-2xl pt-7">Name: Kilani Olamilekan</h2>
+            <h2 className="text-2xl pt-3">Username : @Olamie03</h2>
+            <h3 className="text-2xl pt-3">Web Developer</h3>
+            <h2 className="text-2xl pt-3 pb-3">Phone Number: 090-695-829-40</h2>
+            <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 max-w-70% mt-6" onClick={logout}>LOG OUT</button>
+            <div className="flex flex-row w-3xl justify-center text-center gap-20 mt-8">
+              <button className="justify-center object-center rounded-full  w-12 h-13 bg-stone-100" > <FaWhatsapp size={50} /> </button>
+              <button className="justify-center object-center rounded-full  w-12 h-13 bg-stone-100" ><SiPinetwork size={50} /></button>
+              <button className="justify-center object-center rounded-full  w-12 h-13 bg-stone-100" ><FaTelegram size={50} /></button>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ) : (
+      <div className=" bg-blue-400 flex flex-col justify-center items-center">
+        <h1 className="flex justify-center pt-7.5 font-bold text-4xl text--100">Login Page</h1>
+        <div style={{ display: "flex", flexDirection: "column", gap: 10, padding: 100 }}>
+            <input 
+                className="flex justify-center align-middle border-2 items-center w-100 h-8 "
+                type="text"
+                placeholder="Enter Username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                  checkUsernameValidation(e.target.value);
+                }}
+            />
+            <div>
+                <label>
+                    <input className="text-red-200" disabled type="checkbox" checked={hasNoSymbolErr} /> No Symbols
+                </label>
+                <br />
+                <label>
+                    <input disabled type="checkbox" checked={greaterThanSixErr} /> Length greater 6
+                </label>
+            </div>
+            <input 
+                className="flex items-center justify-center border-2 w-100 h-8"
+                type="password"
+                placeholder="Enter Password"
+                value={password}
+                onChange={(e) => {
+                    setPassword(e.target.value);
+                    checkPasswordValidation(e.target.value);
+                }}
+            />
+            <div className="flex flex-col">
+                <label>
+                    <input disabled type="checkbox" checked={isLongEnough} /> Length greater 6
+                </label>
+                <br />
+                <label>
+                    <input disabled type="checkbox" checked={hasNumber} /> Contains at least one number
+                </label>
+                <br />
+                <label>
+                    <input disabled type="checkbox" checked={hasUpperCase} /> Contains at least one capital letter
+                </label>
+            </div>
+            <button className=" flex flex-row items-center justify-center gap-1 bg-blue-500 text-black font-bold py-3 px-4 rounded hover:bg-blue-700  text-center mt-12" type="button" onClick={login}> LOGIN </button>
+        </div>
+    </div>
+   )}
     </div>
   );
 }
+
